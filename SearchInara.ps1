f446bc3915cad2452b72f7a8c8a9f35aabe00b66
Lukasz Patalas <lpatalas@gmail.com>
2022-10-21 16:51:51 +0200

    Initial commit

---
+++ SearchInara.ps1
@@ -0,0 +1,94 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory)]
+    [ValidateSet('Buy', 'Sell')]
+    [String] $Transaction,
+
+    [Parameter(Mandatory)]
+    [String] $Commodity,
+
+    [String] $NearStarSystem = 'Sol',
+
+    [ValidateSet(0, 1000, 2500, 5000, 10000, 50000)]
+    [String] $MinimumSupplyDemand = 0
+)
+
+Set-StrictMode -Version Latest
+
+$transactionParam = if ($Transaction -eq 'Buy') { '1' } else { '2' }
+$commodityParam = switch ($Commodity) {
+    'Bauxite' { 51 }
+    'Gold' { 42 }
+    'Silver' { 46 }
+    'Tritium' { 10269 }
+    default { throw "Unknown commodity name: $Commodity" }
+}
+$nearStarSystemParam = [System.Web.HttpUtility]::UrlEncode($NearStarSystem)
+$maxStarSystemDistance = 5000
+$maxPriceAgeParam = 24 # pi5=24 (1 day)
+
+$searchUrl = @(
+    'https://inara.cz/elite/commodities/'
+    "?pi1=$transactionParam"
+    "&pi2=$commodityParam"
+    "&ps1=$nearStarSystemParam"
+    "&pi10=1"
+    "&pi11=$maxStarSystemDistance"
+    "&pi3=1"
+    "&pi9=0"
+    "&pi4=0"
+    "&pi5=$maxPriceAgeParam"
+    "&pi12=0"
+    "&pi7=$MinimumSupplyDemand"
+    "&pi8=0"
+) -join ''
+
+Write-Debug "Downloading data from: $searchUrl"
+$wc = New-Object System.Net.WebClient
+$htmlBytes = $wc.DownloadData($searchUrl)
+$html = [System.Text.Encoding]::UTF8.GetString($htmlBytes)
+
+function RemoveTags($text) {
+    $text -replace '<[^>]+>',''
+}
+
+function ParsePrice($text) {
+    if ($text -match '([0-9,\.]+) - ([0-9],\.]+)') {
+        return [decimal]($matches[1])
+    }
+    elseif ($text -match '[0-9,\.]+') {
+        return [decimal]($matches[0])
+    }
+    else {
+        return $null
+    }
+}
+
+function ParseDecimal($text) {
+    if ($text -match '[0-9,\.]+') {
+        return [decimal]($matches[0])
+    }
+    else {
+        return $null
+    }
+}
+
+$matchedCells = $html `
+    | Select-String -Pattern '<td[^>]+>(.+?)</td>' -AllMatches `
+    | Select-Object -ExpandProperty Matches `
+    | ForEach-Object { RemoveTags $_.Groups[1].Value }
+
+$cellsPerRow = 7
+
+for ($cell = 0; $cell -lt $matchedCells.Count; $cell += $cellsPerRow) {
+    [pscustomobject]@{
+        Location = $matchedCells[$cell] -replace '[\ue81d\ufe0e]',''
+        PadSize = $matchedCells[$cell + 1]
+        StationDistance = ParseDecimal ($matchedCells[$cell + 2])
+        SystemDistance = ParseDecimal ($matchedCells[$cell + 3])
+        SupplyDemand = ParseDecimal ($matchedCells[$cell + 4])
+        Price = ParsePrice $matchedCells[$cell + 5]
+        PriceAge = $matchedCells[$cell + 6]
+    }
+}
+
---
+++ ShowDeals.ps1
@@ -0,0 +1,73 @@
+[CmdletBinding()]
+param(
+    [String] $CurrentSystem = 'Sol'
+)
+
+function Buy($commodity, $price) {
+    [pscustomobject]@{
+        Transaction = 'Buy'
+        Commodity = $commodity
+        Price = $price
+        MinimumSupplyDemand = 1000
+    }
+}
+
+function Sell($commodity, $price, $minimumDemand = 1000) {
+    [pscustomobject]@{
+        Transaction = 'Sell'
+        Commodity = $commodity
+        Price = $price
+        MinimumSupplyDemand = $minimumDemand
+    }
+}
+
+$offersToCheck = @(
+    Buy 'Gold' 10000
+    Buy 'Silver' 10000
+    Buy 'Tritium' 30000
+    Sell 'Bauxite' 40000
+    Sell 'Tritium' 200000 -minimumDemand 0
+)
+
+function GetMatchingOffers {
+    $offersChecked = 0
+
+    foreach ($offer in $offersToCheck) {
+        Write-Progress `
+            -Activity 'Checking offers' `
+            -Status "$($offer.Transaction) $($offer.Commodity) at $($offer.Price)" `
+            -PercentComplete ([Math]::Clamp(($offersChecked * 100) / $offersToCheck.Count, 1, 100))
+    
+        $commodities = & "$PSScriptRoot\SearchInara.ps1" `
+            -Transaction $offer.Transaction `
+            -Commodity $offer.Commodity `
+            -MinimumSupplyDemand $offer.MinimumSupplyDemand `
+            -NearStarSystem $CurrentSystem
+    
+        $matchingCommodities = if ($offer.Transaction -eq 'Buy') {
+            $commodities | Where-Object { $_.Price -le $offer.Price }
+        }
+        else {
+            $commodities | Where-Object { $_.Price -ge $offer.Price }
+        }
+    
+        $matchingCommodities `
+        | ForEach-Object {
+            [PSCustomObject]@{
+                Tx = $offer.Transaction
+                Commodity = $offer.Commodity
+                Location = $_.Location
+                Pad = $_.PadSize
+                Distance = $_.SystemDistance
+                'S/D' = $_.SupplyDemand
+                Price = $_.Price
+                Age = $_.PriceAge
+            }
+        }
+    
+        Start-Sleep -Seconds 2
+        $offersChecked += 1
+    }
+}
+
+GetMatchingOffers | Format-Table -AutoSize
